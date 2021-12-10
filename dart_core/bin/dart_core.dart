1abe0b235c201f7b3a6cd0f4b6330e3dc2c7b5ac
unknown <huynhquangkhoi1980@gmail.com>
2021-12-11 06:11:46 +0700

    Add printEvenNumberSumUnder400 function

--- dart_core/bin/dart_core.dart
+++ dart_core/bin/dart_core.dart
@@ -7,14 +7,40 @@ void main(List<String> arguments) {
   // print("Giai thừa của 5 = ${calGiaiThua(5)}");
 
   // Tính Fibonaci
-  calFibonaci(untilNumber: 20000000).toString();
+  // calFibonaci(untilNumber: 20000000).toString();
+
+  // call printEvenNumberSumUnder400() due to print the event number that their sum less than 400
+  printEvenNumberSumUnder400();
 
 
 
 
 
 
+}
 
+/*
+* Từ 1 đến 1000 in ra các số chẵn mà tổng các số đó không lớn hơn 400.
+*/
+void printEvenNumberSumUnder400(){
+  int sum = 0;
+  // creat a list save the event number object
+  List<int> listResult = [];
+  for(int i=0; i<=1000; i++){
+    // If sum > 400 then stop this for loop
+    if (sum > 400) break;
+    // While sum less than 400 and i is a event number then calculate sum for i
+    if ((sum <= 400) && (i%2==0)){
+      sum += i;
+      print(i.toString() + "--" + sum.toString());
+      if(sum <= 400) listResult.add(i);
+    }
+  }
+  
+  for (var j in listResult) {
+    // print all the event number that their sum less than 400
+    print(j.toString());
+  }
 }
 
 // Tính Fibonaci
